5e432c5aaf952f650a0575753c5408e7738d6cb3
emilysaffron <emiy.saffron@bbc.co.uk>
2020-10-28 17:02:02 +0000

    sticky nav

--- src/Navbar.js
+++ src/Navbar.js
@@ -1,11 +1,30 @@
-import React from "react";
+import React, { useEffect } from "react";
 import "./Navbar.css";
 import logo from "./logo.png";
 const Navbar = () => {
+  const [scrolled, setScrolled] = React.useState(false);
+  const handleScroll = () => {
+    const offset = window.scrollY;
+    if (offset > 53) {
+      setScrolled(true);
+    } else {
+      setScrolled(false);
+    }
+  };
+
+  useEffect(() => {
+    window.addEventListener("scroll", handleScroll);
+  });
+  let navbarClasses = ["navbar"];
+  if (scrolled) {
+    navbarClasses.push("scrolled");
+  }
   return (
-    <div className="bar">
-      <img src={logo} alt="logo" />
-    </div>
+    <header className={navbarClasses.join(" ")}>
+      <div className="logo">
+        <img src={logo} alt="logo" />
+      </div>
+    </header>
   );
 };
 
